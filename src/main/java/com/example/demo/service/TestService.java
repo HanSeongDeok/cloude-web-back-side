b5f2eda041ec3500f88819767dbaebfb5fad1f41
sdhan <hsd970730@naver.com>
2025-06-29 16:57:38 +0900

    [SPRING] H2-DB 파일 db로 마이그레이션
    -H2-DB 파일 db로 마이그레이션

--- src/main/java/com/example/demo/service/TestService.java
+++ src/main/java/com/example/demo/service/TestService.java
@@ -1,26 +1,37 @@
 package com.example.demo.service;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.TestUserRequest;
 import com.example.demo.dto.TestUserResponse;
+import com.example.demo.entity.TestEntity;
 import com.example.demo.entity.TestUserEntity;
+import com.example.demo.repository.TestRepository;
 import com.example.demo.repository.TestUserRepository;
 
 @Service
 public class TestService {
     private final String name;
     private final TestUserRepository testUserRepository;
+    private final TestRepository testRepository;
 
     @Autowired
-    public TestService(@Value("${my.name:Han}") String name, TestUserRepository testUserRepository) {
+    public TestService(@Value("${my.name:Han}") String name,
+            TestUserRepository testUserRepository,
+            TestRepository testRepository) {
         this.name = name;
         this.testUserRepository = testUserRepository;
+        this.testRepository = testRepository;
     }
 
     public String test() {
+        testRepository.save(TestEntity.builder()
+                .id(name + UUID.randomUUID().toString())
+                .build());
         return "Hello, " + name;
     }
 
